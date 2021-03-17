766565a51d4870d2f91504585cea0f41fe68ba23
Saul Pinales <saul021002@gmail.com>
2021-03-17 03:46:21 -0400

    faltas

--- main.go
+++ main.go
@@ -6,28 +6,28 @@ import (
 )
 
 func main() {
-	ordena := flag.String("ordena", "", "ordena una palabra o una oracion.")
+	ordena := flag.String("ordena", "", "ordena una palabra o una oración.")
 	flag.Parse()
 	fmt.Println(orderingByte(*ordena))
 
-	revr := flag.String("reverse", "", "imprime una palabra al reves.")
+	revr := flag.String("reverse", "", "imprime una palabra al revés.")
 	flag.Parse()
 	fmt.Println(reverse(*revr))
 
-	primo := flag.Int("primo", -1, "determina si un numero es primo o no.")
+	primo := flag.Int("primo", -1, "determina si un número es primo o no.")
 	flag.Parse()
 	if *primo >= 0 {
 		primos(*primo)
 	}
 
-	fizz := flag.Int("fizzbuzz", 0, `si el numero actual es divisible entre 3, imprimir FIZZ. si el numero actual es divisible entre 5, imprimir BUZZ
-	 si el numero actual de divisible entre 15, imprimir FIZZ BUZZ`)
+	fizz := flag.Int("fizzbuzz", 0, `si el número actual es divisible entre 3, imprimir FIZZ. si el número actual es divisible entre 5, imprimir BUZZ
+	 si el número actual de divisible entre 15, imprimir FIZZ BUZZ`)
 	flag.Parse()
 	if *fizz > 0 {
 		fizzBuzzSecuencia(*fizz)
 	}
 
-	fibo := flag.Int("fibonacci", 0, "secuencia fibonacci, imprime el enesimo numero del numero dado.")
+	fibo := flag.Int("fibonacci", 0, "secuencia fibonacci, imprime el enésimo número del numero dado.")
 	flag.Parse()
 	if *fibo > 0 {
 		fmt.Println(fibonacci(*fibo))
