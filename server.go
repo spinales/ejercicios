ca18db168e966a9560251b5e63308395edd56b48
Saul Pinales <saul021002@gmail.com>
2021-03-20 12:40:44 -0400

    modifing actions

--- server.go
+++ server.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func server() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(rw, "Server basico")
-	})
-
-	// http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files"))))
-	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files"))))
-
-	// http.HandleFunc("/about", func(rw http.ResponseWriter, r *http.Request) {
-	// 	lp := filepath.Join("files", "index.html")
-	// 	fp := filepath.Join("files", filepath.Clean(r.URL.Path))
-
-	// 	tmpl, _ := template.ParseFiles(lp, fp)
-	// 	tmpl.ExecuteTemplate(rw, "index", nil)
-	// })
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./files"))))
 
 	log.Println("app runing :8000")
 	http.ListenAndServe(":8000", nil)
