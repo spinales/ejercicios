d65b3308ab8fa88338385d007279ec05b8e956ce
Saul Pinales <saul021002@gmail.com>
2021-03-20 15:55:45 -0400

    actualizando puerto

--- server.go
+++ server.go
@@ -8,6 +8,6 @@ import (
 func server() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./files"))))
 
-	log.Println("app runing :4000")
-	http.ListenAndServe(":4000", nil)
+	log.Println("app runing :8000")
+	http.ListenAndServe(":8000", nil)
 }
