c5621aa88c20f0f9aba6fd87ce0f6991f358db9f
Saul Pinales <saul021002@gmail.com>
2021-03-22 10:07:06 -0400

    form

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,32 @@ import (
 func server() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./files"))))
 
+	http.HandleFunc("/form", form)
+	http.HandleFunc("/process", formProcess)
+
 	log.Println("app runing :8000")
 	http.ListenAndServe(":8000", nil)
+}
+
+func form(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseGlob("files/*.html"))
+	tpl.ExecuteTemplate(w, "form.html", nil)
+}
+
+func formProcess(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseGlob("files/*.html"))
+
+	if r.Method != "GET" {
+		http.Redirect(w, r, "/form", http.StatusSeeOther)
+	}
+
+	data := struct {
+		Firstname string
+		Lastname  string
+	}{
+		Firstname: r.FormValue("firstname"),
+		Lastname:  r.FormValue("lastname"),
+	}
+
+	tpl.ExecuteTemplate(w, "display.html", data)
 }
