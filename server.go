2fd08e8cb72534d8add29b21b0b244bc79eab43e
Saul Pinales <saul021002@gmail.com>
2021-03-22 16:31:04 -0400

    pagina secreta

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,12 +10,46 @@ import (
 func server() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./files"))))
 
+	http.HandleFunc("/secret", secret)
 	http.HandleFunc("/form", form)
 	http.HandleFunc("/process", formProcess)
 	http.HandleFunc("/home", home)
 
 	log.Println("app runing :8000")
 	http.ListenAndServe(":8000", nil)
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	u, p, ok := r.BasicAuth()
+
+	if !ok {
+		fmt.Println("Error parsing basic auth")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if u != "admin" {
+		fmt.Println("Username provided is incorrect: ", u)
+		http.Redirect(w, r, "/login", http.StatusUnauthorized)
+		return
+	}
+
+	if p != "Pass123" {
+		fmt.Println("Password provided is incorrect: ", p)
+		http.Redirect(w, r, "/login", http.StatusUnauthorized)
+		return
+	}
+
+	d := struct {
+		Tittle  string
+		Content string
+	}{
+		"Pagina secreta",
+		"contenido de pagina secreta",
+	}
+
+	tpl := template.Must(template.ParseGlob("files/*.html"))
+	tpl.ExecuteTemplate(w, "default.html", d)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
